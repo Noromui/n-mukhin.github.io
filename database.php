e1b29652b7216f1f50ea7e3ed7f2cea39d04e161
n-mukhin <145921921+n-mukhin@users.noreply.github.com>
2024-02-08 14:02:21 +0300

    Create database.php

---
+++ database.php
@@ -0,0 +1,32 @@
+<?php
+
+// Подключение к базе данных
+$db = new PDO('mysql:host=localhost;dbname=pvk', 'root', '');
+
+// Функция для получения списка ПВК
+function get_pvk() {
+  global $db;
+
+  $stmt = $db->prepare('SELECT * FROM pvk');
+  $stmt->execute();
+
+  return $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
+
+// Функция для добавления ПВК
+function add_pvk($name) {
+  global $db;
+
+  $stmt = $db->prepare('INSERT INTO pvk (name) VALUES (?)');
+  $stmt->execute([$name]);
+}
+
+// Функция для удаления ПВК
+function delete_pvk($id) {
+  global $db;
+
+  $stmt = $db->prepare('DELETE FROM pvk WHERE id = ?');
+  $stmt->execute([$id]);
+}
+
+?>
